a0cd3f04aebe2d7d5137a86eac8e49cd10f55b6f
Foxytail8 <xinenax@gmail.com>
2026-06-09 11:37:41 +0300

    'Solution'

--- task.sql
+++ task.sql
@@ -1,3 +1,4 @@
+DROP DATABASE ShopDB;
 CREATE DATABASE ShopDB; 
 USE ShopDB; 
 
@@ -9,9 +10,35 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
+CREATE TABLE GeoIPCache (
+    ID INT,
+    IPRange VARCHAR(50),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
+    PRIMARY KEY (ID)
+) ENGINE=Memory;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription (
+    ID INT,
+    CountryID INT,
+    ProductID INT, 
+    Description VARCHAR(100),
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
+    PRIMARY KEY (ID)
+) ENGINE=InnoDB;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE Logs (
+    ID INT,
+    Timestamp DATETIME,
+    Message VARCHAR(500),
+    PRIMARY KEY (ID)
+) ENGINE=Blackhole;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting (
+    `Date` DATE NOT NULL, 
+    ProductName VARCHAR(50) NOT NULL, 
+    Orders INT NOT NULL
+) ENGINE=CSV;
