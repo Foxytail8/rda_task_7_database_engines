eb9ddedc3be3fc01de73aa98793467b2ca437099
Foxytail8 <xinenax@gmail.com>
2026-06-09 11:53:13 +0300

    'Solution4'

--- task.sql
+++ task.sql
@@ -14,15 +14,16 @@ CREATE TABLE GeoIPCache (
     ID INT,
     IPRange VARCHAR(50),
     CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
     PRIMARY KEY (ID)
 ) ENGINE=Memory;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
-    ID INT,
-    CountryID INT,
-    ProductID INT, 
+    ID INT, 
     Description VARCHAR(100),
+    ProductID INT,
+    CountryID INT,    
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
@@ -33,7 +34,7 @@ CREATE TABLE Logs (
     Timestamp DATETIME,
     Message VARCHAR(500),
     PRIMARY KEY (ID)
-) ENGINE=Blackhole;
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
